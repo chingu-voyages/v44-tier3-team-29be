a895c8740e5abb32f02d3978b0936a9085184ef6
devimalka <developerimalka@gmail.com>
2023-05-19 20:59:06 +0530

    feat: Add async for Post CRUD
    - added async for post controller

--- src/controllers/Post.ts
+++ src/controllers/Post.ts
@@ -2,76 +2,97 @@ import { NextFunction, Request, Response } from 'express'
 import { Post } from '../models/schemas/PostSchema'
 
 //create post
-const createPost = (req: Request, res: Response, nex: NextFunction) => {
-  const { title, location, short_description, tags, image, long_description } =
-    req.body
+const createPost = async (req: Request, res: Response, nex: NextFunction) => {
+  try {
+    const {
+      title,
+      location,
+      short_description,
+      tags,
+      image,
+      long_description
+    } = req.body
 
-  const post = new Post({
-    title,
-    location,
-    short_description,
-    tags,
-    image,
-    long_description
-  })
+    const post = new Post({
+      title,
+      location,
+      short_description,
+      tags,
+      image,
+      long_description
+    })
 
-  return post
-    .save()
-    .then((post) => res.status(201).json({ post }))
-    .catch((error) => res.status(500).json({ error }))
+    const savedPost = await post.save()
+
+    return res.status(201).json({ savedPost })
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
 }
 
 // read post by id
-const readPost = (req: Request, res: Response, nex: NextFunction) => {
-  const postId = req.params.postID
+const readPost = async (req: Request, res: Response, nex: NextFunction) => {
+  try {
+    const postId = req.params.postID
 
-  return Post.findById(postId)
-    .then((post) =>
-      post
-        ? res.status(200).json({ post })
-        : res.status(404).json({ message: 'not found' })
-    )
-    .catch((error) => res.status(500).json({ error }))
+    const post = await Post.findById(postId)
+
+    if (!post) {
+      return res.status(404).json({ error: 'Post Not found' })
+    }
+
+    return res.json({ post })
+  } catch (error) {
+    return nex(error)
+  }
 }
 
 // get all posts
-const readAll = (req: Request, res: Response, next: NextFunction) => {
-  return Post.find()
-    .then((posts) => res.status(200).json({ posts }))
-    .catch((error) => res.status(500).json({ error }))
+const readAll = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const posts = await Post.find()
+    return res.status(200).json({ posts })
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
 }
 
 // update post by id
-const updatePost = (req: Request, res: Response, nex: NextFunction) => {
-  const postId = req.params.postID
+const updatePost = async (req: Request, res: Response, nex: NextFunction) => {
+  try {
+    const postId = req.params.postID
 
-  return Post.findById(postId)
-    .then((post) => {
-      if (post) {
-        post.set(req.body)
+    const post = await Post.findById(postId)
 
-        return post
-          .save()
-          .then((post) => res.status(201).json({ post }))
-          .catch((error) => res.status(501).json({ error }))
-      } else {
-        return res.status(404).json({ messge: 'not found' })
-      }
-    })
-    .catch((error) => res.status(500).json({ error }))
+    if (post) {
+      post.set(req.body)
+
+      const updatedPost = await post.save()
+
+      return res.status(201).json({ post: updatedPost })
+    } else {
+      return res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
 }
 
 // delete post by id
-const deletePost = (req: Request, res: Response, nex: NextFunction) => {
-  const postId = req.params.postID
+const deletePost = async (req: Request, res: Response, nex: NextFunction) => {
+  try {
+    const postId = req.params.postID
 
-  return Post.findByIdAndDelete(postId)
-    .then((post) =>
-      post
-        ? res.status(201).json({ post, message: 'Deleted' })
-        : res.status(404).json({ message: 'not found' })
-    )
-    .catch((error) => res.status(500).json({ error }))
+    const deletedPost = await Post.findByIdAndDelete(postId)
+
+    if (deletedPost) {
+      return res.status(201).json({ post: deletedPost, message: 'Deleted' })
+    } else {
+      return res.status(404).json({ message: 'Not found' })
+    }
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
 }
 
 export default { createPost, readPost, updatePost, deletePost, readAll }
